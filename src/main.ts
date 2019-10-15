cb326acd8f6f1efb3fe2319883bb91da7b1df92e
gordon_j_brown <gordonbrownj@gmail.com>
2019-10-15 15:15:09 -0400

    fix regestring off service worker in main.ts

--- src/main.ts
+++ src/main.ts
@@ -8,4 +8,13 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+platformBrowserDynamic().bootstrapModule(AppModule)
+// waiting for angular to load bootstrapping and then only loading service worker
+  .then(()=>{
+    if ('serviceWorker' in navigator){
+    navigator.serviceWorker.register('/ngsw-worker.js')
+    console.log('Registered as service worker');
+    }
+  })
+  .catch(err => console.log(err));
+
