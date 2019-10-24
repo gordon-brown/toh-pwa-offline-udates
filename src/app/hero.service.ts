799220b6bf1cfad640940155b87c1d2c6440e3c9
gordon_j_brown <gordonbrownj@gmail.com>
2019-10-24 16:32:20 -0400

    moved IndexDB stuff into new servie

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,10 +1,10 @@
+import { IndexDbService } from './index-db.service';
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { Observable, of, from } from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
 
-import Dexie from 'dexie'; // wrapper for IndexedDB
 import { Hero } from './hero';
 import { MessageService } from './message.service';
 import { OfflineService } from './offline.service';
@@ -13,7 +13,6 @@ import { OfflineService } from './offline.service';
 export class HeroService {
 
   private heroesUrl = 'api/heroes';  // URL to web api
-  private db: any;
 
   heroes: Hero[];
 
@@ -24,17 +23,18 @@ export class HeroService {
   constructor(
     private readonly offlineService: OfflineService,
     private http: HttpClient,
-    private messageService: MessageService)
+    private messageService: MessageService,
+    private IndexDbService: IndexDbService)
     {
-      this.createIndexedDatabase();
       this.registerToEvents(offlineService);
+      this.IndexDbService.InitializeIndexDB(this.getHeroes());
     }
 
   /** GET heroes from the server */
   getHeroes (): Observable<Hero[]> {
     if (!this.offlineService.isOnline)
     {
-      return from(this.getHeroesFromDB());
+      return from(this.IndexDbService.getHeroesFromDB());
     }
     else {
       return this.http.get<Hero[]>(this.heroesUrl)
@@ -45,31 +45,11 @@ export class HeroService {
     }
   }
 
-  private async getHeroesFromDB(){
-    console.log('getting Heroes from Index DB');
-    return await this.db.hero.toArray();
-  }
-
-  private async getHeroFromDB(id: number){
-    console.log('getting Hero from Index DB');
-    return await await this.db.hero
-      .where('id')
-      .equals(id)
-      .first(function(item) {
-        return item;
-    })
-  }
-
-  private async searchHeroesFromDB(searchTerm: string) {
-    console.log('getting Heroes from Index DB');
-    return await this.db.hero.where("name").startsWith(searchTerm).toArray();;
-  }
-
   /** GET hero by id. Will 404 if id not found */
   getHero(id: number): Observable<Hero> {
     if (!this.offlineService.isOnline)
     {
-      return from(this.getHeroFromDB(id));
+      return from(this.IndexDbService.getHeroFromDB(id));
     }
     else
     {
@@ -89,7 +69,7 @@ export class HeroService {
     }
     if (!this.offlineService.isOnline)
     {
-      return from(this.searchHeroesFromDB(term.trim()));
+      return from(this.IndexDbService.searchHeroesFromDB(term.trim()));
     }
     else {
       return this.http.get<Hero[]>(`${this.heroesUrl}/?name=${term}`).pipe(
@@ -100,81 +80,55 @@ export class HeroService {
   }
 
   //////// Save methods //////////
-/** POST: add a new hero to the server */
+  /** POST: add a new hero to the server */
   addHero (hero: Hero): Observable<Hero> {
 
-    this.AddToHeroAddTable(hero);
+    this.IndexDbService.AddToHeroAddTable(hero, this.offlineService.isOnline);
 
-    return this.AddHeroToDB(hero)
-
-  }
-
-  private AddHeroToDB(hero: Hero): Observable<Hero> {
-    return (this.offlineService.isOnline) ?
-      this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(tap((newHero: Hero) => this.log(`added hero w/ id=${newHero.id}`)), catchError(this.handleError<Hero>('addHero')))
-      :
-      of(hero);
-  }
-
-  private AddToHeroAddTable(hero: Hero) {
-    this.db.hero.orderBy('id').last().then(o => {
-      hero.id = o.id + 1;
-      if (!this.offlineService.isOnline) {
-        this.InsertIntoHeroTransactionTable(hero, 'hero_add');
-      }
-      this.InsertIntoHeroTable(hero);
-    });
-  }
-
-  private AddToHeroUpdateTable(hero: Hero) {
-    if (!this.offlineService.isOnline) {
-      this.InsertIntoHeroTransactionTable(hero, 'hero_update');
+    if (this.offlineService.isOnline) {
+      return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(tap((newHero: Hero) => this.log(`added hero w/ id=${newHero.id}`)), catchError(this.handleError<Hero>('addHero')))
     }
-    this.UpdateFromHeroTable(hero);
+    else {
+       return of(hero);
+    }
   }
 
-  private AddToHeroDeleteTable(hero: Hero) {
-    if (!this.offlineService.isOnline) {
-      this.InsertIntoHeroTransactionTable(hero, 'hero_delete');
-    }
-    this.DeleteFromHeroTable(hero);
-  }
 
   /** DELETE: delete the hero from the server */
   deleteHero (hero: Hero): Observable<Hero> {
 
-    this.AddToHeroDeleteTable(hero);
+    this.IndexDbService.AddToHeroDeleteTable(hero, this.offlineService.isOnline);
 
     return this.DeleteHeroFromDB(hero);
   }
 
   private DeleteHeroFromDB(hero: Hero): Observable<Hero> {
-
-    return (this.offlineService.isOnline) ?
-
-    this.http.delete<Hero>(`${this.heroesUrl}/${hero.id}`, this.httpOptions).pipe(
+    if (this.offlineService.isOnline) {
+      return this.http.delete<Hero>(`${this.heroesUrl}/${hero.id}`, this.httpOptions).pipe(
         tap(_ => this.log(`deleted hero id=${hero.id}`)),
         catchError(this.handleError<Hero>('deleteHero'))
       )
-    :
-      of(hero);
+    }
+    else {
+      return of(hero);
+    }
   }
 
   private UpdateHeroFromDB(hero: Hero): Observable<Hero> {
-    return (this.offlineService.isOnline) ?
-        this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
-          tap(_ => this.log(`updated hero id=${hero.id}`)),
-          catchError(this.handleError<any>('updateHero'))
-        )
-      :
-        of(hero);
+    if (this.offlineService.isOnline) {
+      return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
+        tap(_ => this.log(`updated hero id=${hero.id}`)),
+        catchError(this.handleError<any>('updateHero'))
+      )
+    }
+    else {
+      return of(hero);
+    }
   }
-
 
   /** PUT: update the hero on the server */
   updateHero (hero: Hero): Observable<any> {
-    this.AddToHeroUpdateTable(hero);
-
+    this.IndexDbService.AddToHeroUpdateTable(hero, this.offlineService.isOnline);
     return this.UpdateHeroFromDB(hero);
   }
 
@@ -206,11 +160,7 @@ export class HeroService {
   private registerToEvents(offlineService: OfflineService) {
     offlineService.connectionChanged.subscribe(online => {
       if (online) {
-        console.log('went online');
-        console.log('sending all stored items');
-
-        //pass the items to the backend if the connetion is enabled
-        // todo redo this
+        console.log('went online, sending all stored items');
         this.sendItemsFromIndexedDb();
       } else {
         console.log('went offline, storing in indexdb');
@@ -219,111 +169,41 @@ export class HeroService {
   }
 
   async sendItemsFromIndexedDb() {
-    console.log('Inserting into db from hero_add');
 
-    var items = await this.db.hero_add.toArray();
-    items.forEach(h => {
-      this.AddHeroToDB(h);
-    })
+    console.log('Sending items from IndexDB');
 
-    this.db.hero_add.clear();
-
-    console.log('update from hero_update');
-
-    items = await this.db.hero_update.toArray();
-    items.forEach(h => {
-      this.UpdateHeroFromDB(h);
-    })
-
-    this.db.hero_update.clear();
-
-    console.log('deleting from hero_delete');
-
-    items = await this.db.hero_delete.toArray();
-    items.forEach(h => {
-      this.DeleteHeroFromDB(h);
-    })
-
-    this.db.hero_delete.clear();
-
-  }
-
-  // ---------- create the hero database
-  private createIndexedDatabase(){
-
-    console.log('creating Hero DB');
-    let db_name = "hero_database"
-    this.db = new Dexie(db_name);
-    this.CreateTables();
-
-    this.db.open()
-      .then(() => {
-        console.log('opened database ' + db_name)
-        this.InitializeHeroTable();
-      })
-      .catch(function (err) {
-        console.error (err.stack || err);
-      });
-
-  }
-
-  private CreateTables() {
-    this.db.version(1).stores({
-      hero: "id,name",
-      hero_add: "id",
-      hero_update: "id",
-      hero_delete: "id"
-    });
-  }
-
-  private InitializeHeroTable() {
-    this.getHeroes().subscribe(h => {
-      console.log('populate heroes DB');
-      this.heroes = h;
-      this.ClearTables();
-      var i: number;
-      for (i = 0; i < this.heroes.length; i++) {
-        this.InsertIntoHeroTable(this.heroes[i]);
+    await this.IndexDbService.GetHeroesToAdd().then(heroes =>
+      {
+        heroes.forEach(hero => {
+          this.addHero(hero).subscribe(() =>
+            console.log('Added ' + JSON.stringify(hero))
+          );
+        })
       }
-    });
-  }
+     );
 
-  private ClearTables() {
-    this.db.hero.clear();
-    this.db.hero_add.clear();
-    this.db.hero_update.clear();
-    this.db.hero_delete.clear();
-  }
+    await this.IndexDbService.GetHeroesToUpdate().then(heroes =>
+      {
+        heroes.forEach(hero => {
+          this.updateHero(hero).subscribe(() =>
+            console.log('Updated ' + JSON.stringify(hero))
+          );
+       })
+      }
+     );
 
-  private InsertIntoHeroTable(hero: Hero) {
-    this.db.table('hero').add({id: hero.id, name: hero.name})
-      .catch(e => {
-        console.error('Insert Into Heroes Error: ' + (e.stack || e));
-    });
-  }
+    await this.IndexDbService.GetHeroesToDelete().then(heroes =>
+      {
+        heroes.forEach(hero => {
+          this.deleteHero(hero).subscribe(() =>
+            console.log('Deleted ' + JSON.stringify(hero))
+          );
+        })
+      }
+     );
 
-  private UpdateFromHeroTable(hero: Hero) {
-    this.db.table('hero').update(hero.id, {name: hero.name})
-    .catch(e => {
-      console.error('Update Into Heroes Error: ' + (e.stack || e));
-    });
-  }
+    this.IndexDbService.ClearTables();
 
-  private DeleteFromHeroTable(hero: Hero) {
-    this.db.table('hero').where('id').equals(hero.id).delete()
-      .catch(e => {
-        console.error('Delete from Heroes Error: ' + (e.stack || e));
-    });
-  }
-
-  private InsertIntoHeroTransactionTable(hero: Hero, tableName: string) {
-    this.db.table(tableName).add({id: hero.id, name: hero.name})
-      .then(() => {
-        console.log('Table ' + tableName + ' ' + hero.name);
-      })
-      .catch(e => {
-        console.error('Insert Into ' + tableName + 'Error:' + (e.stack || e));
-    });
   }
 
 }
